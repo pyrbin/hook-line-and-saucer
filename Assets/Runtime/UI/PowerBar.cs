85a0a5ed5cbbed07218ee80a563c1ede4c65a1a2
Frans B <frans.bstrom@gmail.com>
2021-10-17 01:51:21 +0200

    integrated HUD to game scene

--- Assets/Runtime/UI/PowerBar.cs
+++ Assets/Runtime/UI/PowerBar.cs
@@ -5,12 +5,10 @@ public class PowerBar : MonoBehaviour
 {
     public DragPower dragPower;
     public Image MaxPower;
-    public Image PowerUsage;
 
     // Update is called once per frame
     void Update()
     {
-        MaxPower.fillAmount = dragPower.currentPower/dragPower.MaxPower;
-        PowerUsage.fillAmount = dragPower.PowerUsage/dragPower.MaxPower;
+        MaxPower.fillAmount = dragPower.PowerUsage / dragPower.MaxPower;
     }
 }
