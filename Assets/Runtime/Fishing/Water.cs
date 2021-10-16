22d187593a7b1d8cb1a7dbfe07696abde20016dd
smultronbusken <fredholmisak@outlook.com>
2021-10-16 12:56:34 +0200

    bait properly works

--- Assets/Runtime/Fishing/Water.cs
+++ Assets/Runtime/Fishing/Water.cs
@@ -1,28 +1,26 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Unity.Mathematics;
 
 public class Water : MonoBehaviour
 {
 
-    PhysicsEvents2D physicsEvents;
-
     // Start is called before the first frame update
     void Start()
     {
-        TryGetComponent(out physicsEvents);
+        TryGetComponent<PhysicsEvents2D>(out var physicsEvents);
+
         physicsEvents.TriggerEnter += (collision) => {
-            collision.gameObject.TryGetComponent<Bait>(out var bait);
-            if (bait) {
-                Debug.Log("Entered " + bait);
-            }
+            collision.TryGetComponent<Bait>(out var bait);
+            bait.setInWater();
+            bait.reelDirection = new float2(0.1f, 1f);
         };  
 
         physicsEvents.TriggerExit += (collision) => {
-            collision.gameObject.TryGetComponent<Bait>(out var bait);
-            if (bait) {
-                Debug.Log("Entered " + bait);
-            }
+            collision.TryGetComponent<Bait>(out var bait);
+            bait.reelDirection = new float2(0.1f, 0);
+            bait.body.velocity =  new Vector2(bait.body.velocity.x, 0);
         };  
     }
 
